ddc5a94179d5f14621d3a407a212ce5c02cc5968
TrungHT <trongtrungak@gmail.com>
2023-03-16 17:05:03 +0700

    Config Kubernetes

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a5185424f875a4a53a026dd378207056-864516943.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
